403f4828e7f4e6fb56902a96ca0e4cbb5648830c
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-05-15 22:45:50 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Example tutorial
     var tutorial = document.createElement('article');
     var title = document.createElement('h3');
-    title.textContent = 'Example Tutorial';
+    title.textContent = 'Gonna put tutorials here';
     var description = document.createElement('p');
-    description.textContent = 'This is an example tutorial.';
+    description.textContent = '-- Placeholder --';
     description.className = 'description';
     var body = document.createElement('pre');
     body.className = 'language-js';
@@ -14,9 +13,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
     tutorial.appendChild(body);
     document.getElementById('tutorials').appendChild(tutorial);
 
-    // Example bookmarklet
     var bookmarklet = document.createElement('pre');
     bookmarklet.className = 'language-js';
-    bookmarklet.innerHTML = '<code class="language-js">alert("This is a bookmarklet!");</code>';
+    bookmarklet.innerHTML = '<code class="language-js">alert("This is a placeholder!");</code>';
     document.getElementById('bookmarklets').appendChild(bookmarklet);
 });
